6b28608b346bf237efac9357b9ad95c94d032993
t0t00ns <timothy.tse@walmartlabs.com>
2020-11-27 20:25:59 -0800

    811. Subdomain Visit Count https://leetcode.com/problems/subdomain-visit-count/

---
+++ src/main/java/SubdomainVisitCount.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class SubdomainVisitCount {
+    public List<String> subdomainVisits(String[] cpdomains) {
+        Map<String, Integer> domainCounter = new HashMap<>();
+
+        for (String cpdomain : cpdomains) {
+            int splitIdx = cpdomain.indexOf(" ");
+            Integer count = Integer.valueOf(cpdomain.substring(0, splitIdx));
+            String domain = cpdomain.substring(splitIdx + 1);
+            for (int i = domain.length() - 1; i >= 0; --i) {
+                if (domain.charAt(i) == '.') {
+                    String subdomain = domain.substring(i + 1);
+                    domainCounter.merge(subdomain, count, Integer::sum);
+                }
+            }
+            domainCounter.merge(domain, count, Integer::sum);
+        }
+
+        return domainCounter.entrySet().stream()
+                .map(domainCountKvPair -> domainCountKvPair.getValue().toString() + " " + domainCountKvPair.getKey())
+                .collect(Collectors.toList());
+    }
+}
